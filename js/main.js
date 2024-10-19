17cdb290a04fbd21c31e8f485872e0c17aa9d6f0
Neraxis_1210 <99865968+Phong12HexDockwork@users.noreply.github.com>
2024-10-20 06:03:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Girl day 20.10 ').split('')
+    const titles = ('Happy Women day 20.10 ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
