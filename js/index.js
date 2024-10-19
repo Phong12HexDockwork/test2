4b5c23faa3ec3b35de79a9e2009427f6dbd2cebe
Neraxis_1210 <99865968+Phong12HexDockwork@users.noreply.github.com>
2024-10-19 23:49:29 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = '=)))))) Nhma xin 500'.split('')
+const text = 'He he he he'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
